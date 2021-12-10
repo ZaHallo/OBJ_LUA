5c96b93a4cf9ac53488dbd4e4f00abb3f60adf5e
ZaHallo <95763671+ZaHallo@users.noreply.github.com>
2021-12-10 11:17:27 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,4 +1,7 @@
 Enum.ClassAttributes = Instance.new("EnumCategory")
 local abstract = Instance.new("EnumValue")
 abstract.Name = "Abstract"
+local empty = Instance.new("EnumValue")
+empty.Name = "Empty"
 Enum.ClassAttributes:NewItem(abstract)
+Enum.ClassAttributes:NewItem(empty)
