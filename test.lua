7441839d063c2ab269a933300b83369fd32e7934
ZaHallo <95763671+ZaHallo@users.noreply.github.com>
2021-12-10 10:29:00 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,29 @@
 Class.new("Instance"){}
+
+Class.new("EnumValue"){
+  fields = {
+		Value = 0;
+		Name = "";
+	};
+}
+Class.new("EnumCategory"){
+  fields = {
+		EnumValues = {};
+  };
+  methods = {
+    NewItem = function(self,EnumValue)
+			self.EnumValues[EnumValue.Name] = EnumValue
+    end;
+		GetItem = function(self,NewItemString)
+      return self.EnumValues[NewItemString]
+    end;
+		GetAllItems = function(self)
+      return self.EnumValues
+    end;
+  };
+}
+
+Enum.ClassAttributes = Instance.new("EnumCategory")
+local abstract = Instance.new("EnumValue")
+abstract.Name = "Abstract"
+Enum.ClassAttributes:NewItem(abstract)
